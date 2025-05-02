d2ebcca71e3fe0f024abdefb7ec53c4cbda416d0
Anant <anant.maheshwary@gmail.com>
2025-05-02 12:51:27 +0530

    compile.ml done

--- src/miniml/compile.ml
+++ src/miniml/compile.ml
@@ -16,3 +16,11 @@ let rec compile {Zoo.data=e'; _} =
     | Syntax.If (e1, e2, e3) -> (compile e1) @ [IBranch (compile e2, compile e3)]
     | Syntax.Fun (f, x, _, _, e) -> [IClosure (f, x, compile e @ [IPopEnv])]
     | Syntax.Apply (e1, e2) -> (compile e1) @ (compile e2) @ [ICall]
+    | Syntax.Divide (e1, e2) -> (compile e1) @ (compile e2) @ [IDiv]
+    
+    (* These recursively break the expression into simpler expressions using pattern matching and then call the compile function recursively. *)
+    (* All the resulting frames (list of instructions) are then concatenated into a frame a single list of instructions *)
+    | Syntax.TryWith (e1, handlers) -> (compile e1) @ [ IHandle ( List.map (fun (ex , e2) -> (ex, compile(e2)) ) handlers ) ] 
+    | Syntax.Exception (ex) -> [IExp ex]
+    | Syntax.Raise (ex) -> [IRaise ex]
+  
